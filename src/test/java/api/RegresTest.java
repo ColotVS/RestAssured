f31a977dc1de1c695a8ab5fc8c5fc0aa31de8ff5
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-17 13:17:15 +0700

    Написание проверки имена файлов аватаров пользователей совпадают с ID.

--- src/test/java/api/RegresTest.java
+++ src/test/java/api/RegresTest.java
@@ -18,9 +18,9 @@ public class RegresTest {
         List<UserData> users = given()  //Статичный метод given() RestAssured
                 .when()
                 .contentType(ContentType.JSON) //Выбор типа ответа
-                .get(URL+"/api/users?page=2")//Указываем запрос и ссылку
+                .get(URL + "/api/users?page=2")//Указываем запрос и ссылку
                 .then().log().all()             //Выводим лог в консоль
-                .extract().body().jsonPath().getList("data", UserData.class); //Извлекаем Json в класс UserData
+                .extract().body().jsonPath().getList("data", UserData.class); //Извлекаем Json в формате UserData
         users.forEach(x-> Assert.assertTrue(x.getAvatar().contains(x.getId().toString())));
     }
 }

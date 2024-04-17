36bc0f93266f8eab4d3d6e0f7bb596e2d6a15543
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-17 17:11:49 +0700

    Сокращение кода с использованием Specification

--- src/test/java/api/RegresTest.java
+++ src/test/java/api/RegresTest.java
@@ -16,10 +16,10 @@ public class RegresTest {
     private static final String URL = "https://reqres.in";
     @Test
     public void checkAvatarAndIdTest(){
+        Specification.installSpecification(Specification.requestSpecification(URL),Specification.responseSpecificationOK200());
         List<UserData> users = given()  //Статичный метод given() RestAssured
                 .when()
-                .contentType(ContentType.JSON) //Выбор типа ответа
-                .get(URL + "/api/users?page=2")//Указываем запрос и ссылку
+                .get("/api/users?page=2")//Указываем запрос и ссылку
                 .then().log().all()             //Выводим лог в консоль
                 .extract().body().jsonPath().getList("data", UserData.class); //Извлекаем Json в формате UserData
         //users.forEach(x-> Assert.assertTrue(x.getAvatar().contains(x.getId().toString()))); //Убедиться что имена файлов аватаров пользователей включают ID пользователей.

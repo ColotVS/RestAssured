50815d0a21171ea9ba83babf3a5482a320016edb
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-17 17:06:49 +0700

    ResponseSpecification

--- src/test/java/api/Specification.java
+++ src/test/java/api/Specification.java
@@ -1,8 +1,11 @@
 package api;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 
 
 public class Specification {
@@ -10,6 +13,11 @@ public class Specification {
         return new RequestSpecBuilder()
                 .setBaseUri(url)
                 .setContentType(ContentType.JSON)
+                .build();
+    }
+    public static ResponseSpecification responseSpecificationOK200(){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(200)
                 .build();
     }
 }

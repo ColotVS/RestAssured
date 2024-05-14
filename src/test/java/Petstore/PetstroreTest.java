55f49844dc6df977a8ae82449dea3381840cf6e7
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-05-14 13:29:55 +0700

    Создаём метод добавление нового животного, позитивный сценарий

--- src/test/java/Petstore/PetstroreTest.java
+++ src/test/java/Petstore/PetstroreTest.java
@@ -1,5 +1,18 @@
 package Petstore;
 
+import api.Specification;
+import org.junit.Test;
+
 public class PetstroreTest {
     private final static String URL = "https://petstore.swagger.io/v2";
+
+    /**
+     * Работа с методом POST
+     * Добавление нового животного
+     * Позитивный сценарий
+     */
+    @Test
+    public void checkAddNewPetTest(){
+        Specification.installSpecification(Specification.requestSpecification(URL),Specification.responseSpecificationUnique(200));
+    }
 }

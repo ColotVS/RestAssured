0d312d148030f5570689a95ebc2de3d1d9af3932
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-05-14 14:07:23 +0700

    Создаём pogo-класс регистрации

---
+++ src/test/java/Petstore/PetRegister.java
@@ -0,0 +1,40 @@
+package Petstore;
+
+public class PetRegister {
+    private Integer id;
+    private String name;
+    private String status;
+
+    public PetRegister(Integer id, String name, String status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+
+    public PetRegister() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}

9c89e88a78fccc2a7436772174524b6a49f490ec
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-05-14 14:29:03 +0700

    Отправка POST запроса

--- src/test/java/Petstore/PetstroreTest.java
+++ src/test/java/Petstore/PetstroreTest.java
@@ -3,6 +3,8 @@ package Petstore;
 import api.Specification;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class PetstroreTest {
     private final static String URL = "https://petstore.swagger.io/v2";
 
@@ -14,5 +16,11 @@ public class PetstroreTest {
     @Test
     public void checkAddNewPetTest(){
         Specification.installSpecification(Specification.requestSpecification(URL),Specification.responseSpecificationUnique(200));
+        PetRegister pet = new PetRegister(123,"Шери","available");
+        given()
+                .body(pet)
+                .when()
+                .post("/pet")
+                .then().log().all();
     }
 }
